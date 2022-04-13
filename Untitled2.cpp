0e90b925f99bdd6112c31c3e259818146181200b
asuyazilimlab@outlook.com <asuyazilimlab@outlook.com>
2022-04-13 14:35:57 +0300

    fonksiyon

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
+#include <iostream>
+using namespace std;
 
-int main() {
-	cout << "hello";
+int alanHesapla(int a, int b) {
+
+	return a * b;
 }
-	
+int cevreHesapla(int a, int b) {
+	return 2 * (a + b);
+}
+int main() {
+	int kkenar, UKenar;
+	cout << "uzun kenari giriniz: ";
+	cin >> kkenar;
+	cout << "kisa kenari giriniz: ";
+	cin >> UKenar;
+	cout << "dikdortgenin alani: " << alanHesapla(kkenar, UKenar) << endl;
+	cout << "dikdortgenin cevresi: " << cevreHesapla(kkenar, UKenar);
+}
+
+
+
 
 
